8c559d8cd09c924d36aab1bd6aa8b8da2eb8ae2f
pablo putin <pablopuntin@gmail.com>
2025-10-01 20:22:21 -0300

    modulosempleadoy empresa corregidos, entidades agregadas al appModule

--- src/empleado/empleado.module.ts
+++ src/empleado/empleado.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Empleado } from './entities/empleado.entity';
 import { EmpleadoService } from './empleado.service';
 import { EmpleadoController } from './empleado.controller';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Empleado])],
   controllers: [EmpleadoController],
-  providers: [EmpleadoService],
+  providers: [EmpleadoService]
 })
 export class EmpleadoModule {}
--- src/empresa/empresa.module.ts
+++ src/empresa/empresa.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Empresa } from './entities/empresa.entity';
 import { EmpresaService } from './empresa.service';
 import { EmpresaController } from './empresa.controller';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Empresa])],
   controllers: [EmpresaController],
-  providers: [EmpresaService],
+  providers: [EmpresaService]
 })
 export class EmpresaModule {}
