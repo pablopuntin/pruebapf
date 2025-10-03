2d58197f3250af384631fff2565375adf34e9ed1
pablo putin <pablopuntin@gmail.com>
2025-10-02 23:24:36 -0300

    entidades y relaciones01

--- src/empleado/dto/create-empleado.dto.ts
+++ src/empleado/dto/create-empleado.dto.ts
@@ -1,3 +1,80 @@
+// import { ApiProperty } from '@nestjs/swagger';
+// import {
+//   IsString,
+//   IsNotEmpty,
+//   IsInt,
+//   IsOptional,
+//   IsDateString,
+//   IsNumber,
+//   Matches,
+//   IsEmail
+// } from 'class-validator';
+
+// export class CreateEmployeeDto {
+//   @ApiProperty({ example: 'Juan', description: 'Nombre del empleado' })
+//   @IsString()
+//   @IsNotEmpty()
+//   first_name: string;
+
+//   @ApiProperty({ example: 'Pérez', description: 'Apellido del empleado' })
+//   @IsString()
+//   @IsNotEmpty()
+//   last_name: string;
+
+//   @ApiProperty({ example: 30, description: 'Edad del empleado' })
+//   @IsInt()
+//   @IsOptional()
+//   age?: number;
+
+//   @ApiProperty({ example: 12345679, description: 'DNI único del empleado' })
+//   @IsInt()
+//   dni: number;
+
+//   @ApiProperty({ example: '20-12345678-9', description: 'CUIL único del empleado' })
+//   @IsString()
+//   @IsNotEmpty()
+//   cuil: string;
+
+//   @ApiProperty({ example: '+54 9 11 1234-5678', required: false })
+//   @IsOptional()
+//   @IsString()
+//   phone_number?: string;
+
+//   @ApiProperty({ example: 'Av. Siempre Viva 123', required: false })
+//   @IsOptional()
+//   @IsString()
+//   address?: string;
+
+//   @ApiProperty({ example: '1995-05-12', description: 'Fecha de nacimiento', required: false })
+//   @IsOptional()
+//   @IsDateString()
+//   birthdate?: Date;
+
+//   @ApiProperty({ example: 'https://cdn.com/empleado.png', required: false })
+//   @IsOptional()
+//   @IsString()
+//   imgUrl?: string;
+
+//   @ApiProperty({
+//     example: 75000.5,
+//     description: 'Sueldo del empleado',
+//     required: false,
+//     type: 'number',
+//     format: 'float'
+//   })
+//   @Matches(/^\d+(\.\d{1,2})?$/, {
+//     message: 'El sueldo debe tener como máximo dos decimales'
+//   })
+//   @IsOptional()
+//   @IsNumber()
+//   salary?: number;
+
+//   @ApiProperty({ example: 'juan.perez@email.com', description: 'Correo electrónico del empleado' })
+//   @IsEmail()
+//   @IsNotEmpty()
+//   email: string;
+// }
+
 import { ApiProperty } from '@nestjs/swagger';
 import {
   IsString,
@@ -6,9 +83,9 @@ import {
   IsOptional,
   IsDateString,
   IsNumber,
-  Matches,
   IsEmail
 } from 'class-validator';
+import { MaxTwoDecimals } from 'src/validators/max-two-decimals.validator'; // import del custom validator
 
 export class CreateEmployeeDto {
   @ApiProperty({ example: 'Juan', description: 'Nombre del empleado' })
@@ -62,11 +139,9 @@ export class CreateEmployeeDto {
     type: 'number',
     format: 'float'
   })
-  @Matches(/^\d+(\.\d{1,2})?$/, {
-    message: 'El sueldo debe tener como máximo dos decimales'
-  })
   @IsOptional()
   @IsNumber()
+  @MaxTwoDecimals({ message: 'El sueldo debe tener como máximo dos decimales' })
   salary?: number;
 
   @ApiProperty({ example: 'juan.perez@email.com', description: 'Correo electrónico del empleado' })
