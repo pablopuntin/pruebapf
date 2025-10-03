2573a73336df075a57a75cf0551ba08513b724c8
Jesus May Iuit <jmi_1416@hotmail.com>
2025-10-03 08:09:55 -0600

    Modificacion al guardian de autentificacion

--- src/auth/guards/auth0db.guard.ts
+++ src/auth/guards/auth0db.guard.ts
@@ -3,15 +3,20 @@ import {
   ExecutionContext,
   Injectable,
   UnauthorizedException,
-  ForbiddenException,
+  ForbiddenException
 } from '@nestjs/common';
 import { Request, Response } from 'express';
 import { requiresAuth } from 'express-openid-connect';
-//import { UsersService } from './users.service'; // tu servicio de DB
+import { Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from 'src/user/entities/user.entity';
 
 @Injectable()
 export class Auth0DbGuard implements CanActivate {
-  //constructor(private readonly usersService: UsersService) {}
+  constructor(
+    @InjectRepository(User)
+    private readonly usersRepository: Repository<User>
+  ) {}
 
   async canActivate(context: ExecutionContext): Promise<boolean> {
     const httpContext = context.switchToHttp();
@@ -31,13 +36,15 @@ export class Auth0DbGuard implements CanActivate {
     if (!auth0User) {
       throw new UnauthorizedException('No user data from Auth0');
     }
-    /*
+
     // 2. Validar en DB
-    const dbUser = await this.usersService.findByEmail(auth0User.email);
+    const dbUser = await this.usersRepository.findOne({
+      where: { email: auth0User.email }
+    });
     if (!dbUser) {
       throw new ForbiddenException('User not registered in the database');
     }
-*/
+
     return true; //pasa Auth0 + DB
   }
 }
