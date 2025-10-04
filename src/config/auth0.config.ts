6a41a7a09ad4daecc89a940f51324190b9cab939
Jesus May Iuit <jmi_1416@hotmail.com>
2025-10-04 14:38:11 -0600

    Se cambio la configuracion de auth0

--- src/config/auth0.config.ts
+++ src/config/auth0.config.ts
@@ -1,9 +1,17 @@
+import { ConfigParams } from 'express-openid-connect';
 import { AUTH0_BASEURL, AUTH0_CLIENTID, AUTH0_SECRET } from './auth0.envs';
-export const config = {
+export const config: ConfigParams = {
   authRequired: false,
   auth0Logout: true,
   secret: AUTH0_SECRET,
   baseURL: AUTH0_BASEURL ?? 'http://localhost:3000',
   clientID: AUTH0_CLIENTID,
   issuerBaseURL: 'https://dev-hrsystem.us.auth0.com',
+  routes: {
+    callback: '/callback'
+  },
+  afterCallback: async (req, res, session) => {
+    res.redirect('https://front-git-main-hr-systems-projects.vercel.app/');
+    return session;
+  }
 };
