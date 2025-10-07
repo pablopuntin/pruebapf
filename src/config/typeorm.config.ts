ed88d16a3d3873df6d788987360cbe3dfb5cf991
pablo putin <pablopuntin@gmail.com>
2025-10-07 12:42:56 -0300

    refact typeorm.config

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -1,25 +1,54 @@
-// src/config/typeorm.config.ts
+// // src/config/typeorm.config.ts
+// import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+// import { ConfigService } from '@nestjs/config';
+
+// export const getTypeOrmConfig = (
+//   config: ConfigService
+// ): TypeOrmModuleOptions => {
+//   // Agrega los console.log aquí
+//   console.log('DB_USER:', config.get<string>('DB_USER'));
+//   console.log('DB_PASSWORD:', config.get<string>('DB_PASSWORD'));
+//   console.log('DB_HOST:', config.get<string>('DB_HOST'));
+//   return {
+//     type: 'postgres',
+//     url: config.get<string>('DATABASE_URL') || undefined,
+//     host: config.get<string>('DB_HOST'),
+//     port: config.get<number>('DB_PORT') || 5432,
+//     username: config.get<string>('DB_USERNAME'),
+//     password: config.get<string>('DB_PASSWORD'),
+//     database: config.get<string>('DB_NAME'),
+//     ssl: config.get('DATABASE_URL') ? { rejectUnauthorized: false } : false,
+//     autoLoadEntities: true,
+//     synchronize: false, //true borra todo
+//     dropSchema: false // ⚠️ Nunca usar true en producción
+//   };
+// };
+
+//refact
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 import { ConfigService } from '@nestjs/config';
 
 export const getTypeOrmConfig = (
   config: ConfigService
 ): TypeOrmModuleOptions => {
-  // Agrega los console.log aquí
+  const databaseUrl = config.get<string>('DATABASE_URL');
+
+  console.log('📦 DB Config');
   console.log('DB_USER:', config.get<string>('DB_USER'));
   console.log('DB_PASSWORD:', config.get<string>('DB_PASSWORD'));
   console.log('DB_HOST:', config.get<string>('DB_HOST'));
+
   return {
     type: 'postgres',
-    url: config.get<string>('DATABASE_URL') || undefined,
+    url: databaseUrl || undefined,
     host: config.get<string>('DB_HOST'),
     port: config.get<number>('DB_PORT') || 5432,
     username: config.get<string>('DB_USERNAME'),
     password: config.get<string>('DB_PASSWORD'),
     database: config.get<string>('DB_NAME'),
-    ssl: config.get('DATABASE_URL') ? { rejectUnauthorized: false } : false,
+    ssl: databaseUrl ? { rejectUnauthorized: false } : false,
     autoLoadEntities: true,
-    synchronize: false, //true borra todo
-    dropSchema: false // ⚠️ Nunca usar true en producción
+    synchronize: false,
+    dropSchema: false
   };
 };
