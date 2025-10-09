074808a59cbe3e2b3bd359b9d142aa09ab1f12c2
Jesus May Iuit <jmi_1416@hotmail.com>
2025-10-08 19:52:35 -0600

    Middleware cookie

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,11 @@
 // src/app.module.ts
-import { Module } from '@nestjs/common';
+import {
+  Module,
+  NestModule,
+  MiddlewareConsumer,
+  RequestMethod
+} from '@nestjs/common';
+import { SameSiteNoneMiddleware } from './middlewares/samesite.middleware';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { getTypeOrmConfig } from './config/typeorm.config';
@@ -67,4 +73,10 @@ import { Position } from './position/entities/position.entity';
     PositionService
   ]
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(SameSiteNoneMiddleware)
+      .forRoutes({ path: '*', method: RequestMethod.ALL }); // Aplica a todas las rutas
+  }
+}
